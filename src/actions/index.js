074fe5a43618b950751d5eeb967498f2665bce8c
Scarlett <scarlett.truong307@gmail.com>
2018-08-27 00:11:52 -0700

    redux-promise: console 'request & action'

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = '9b4351c72e523627ee41f3d1fc568346';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},ca`
+    const request = axios.get(url);
+    
+    console.log('Request: ' + request);
 
     return {
         type: FETCH_WEATHER,
-
+        payload: request
     }
 }
